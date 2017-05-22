d65069b7f85a2bb8f26a8c03da78e47581fc7d57
Ash Wilson <smashwilson@github.com>
2017-05-22 16:29:44 -0400

    Track prefix and bin directories

--- versions.rb
+++ versions.rb
@@ -5,7 +5,8 @@ GPG_VERSIONS = %w(1.2.0 1.4.21 2.0.29 2.1.21)
 GPG_VERSION_DIRS = Hash[GPG_VERSIONS.map do |version|
   root_dir = File.join(__dir__, '.gpg', version)
   src_dir = File.join(root_dir, 'src')
-  bin_dir = File.join(root_dir, 'bin')
+  out_dir = File.join(root_dir, 'out')
+  bin_dir = File.join(out_dir, 'bin')
 
-  [version, {bin: bin_dir, src: src_dir}]
+  [version, {out: out_dir, src: src_dir, bin: bin_dir}]
 end]
